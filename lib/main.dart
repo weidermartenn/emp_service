2c3bfa1e4969552f32c7852ad35834484e29cb47
weidermartenn <weidermartenn@gmail.com>
2024-04-21 19:27:20 +0300

    shjdf

--- lib/main.dart
+++ lib/main.dart
@@ -31,39 +31,103 @@ class MainApp extends StatelessWidget {
                   ),
                 ),
                 child: Center( 
-                  child: Column( 
-                    mainAxisAlignment: MainAxisAlignment.start,
+                  child: Column(                              
                     children: [
                       ConstrainedBox( 
                         constraints: BoxConstraints(
                           maxWidth: constraints.maxWidth,
-                          maxHeight: 100,
+                          maxHeight: 200,
                         ),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            color: Color.fromARGB(188, 0, 0, 0),                      
-                          ),
+                        child: Container( 
+                          decoration: BoxDecoration(
+                            color: Colors.black12
+                          ),                        
                           child: Center(
                             child: Row( 
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [ 
                                 Row (
                                   children: [ 
-                                    Image.asset('assets/icons/default_icon.png', width: 80, height: 80),
-                                    const Padding( 
-                                      padding: EdgeInsets.only(top: 15),
-                                      child: Text('СЛУЖБА ЗАНЯТОСТИ', 
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                        color: Colors.white, 
-                                        fontSize: 40, 
-                                        fontWeight: FontWeight.w500
-                                      ),
-                                    ),
+                                    Column( 
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Row( 
+                                          children: [
+                                            Image.asset('assets/icons/default_icon.png', width: 80, height: 80),
+                                            const Padding( 
+                                              padding: EdgeInsets.only(top: 15),
+                                              child: Text('СЛУЖБА ЗАНЯТОСТИ', 
+                                              textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                color: Colors.white, 
+                                                fontSize: 40, 
+                                                fontWeight: FontWeight.w500
+                                              ),
+                                            ),
+                                            ),   
+                                          ]
+                                        ),
+                                        Row( 
+                                          children: [
+                                            TextButton(
+                                              child: const Text('Войти'),
+                                              onPressed: () => showDialog<String>(
+                                              context: context,
+                                              builder: (BuildContext context) => Dialog(
+                                                child: Padding(
+                                                  padding: const EdgeInsets.all(8.0),
+                                                  child: Column(
+                                                    mainAxisSize: MainAxisSize.min,
+                                                    mainAxisAlignment: MainAxisAlignment.center,
+                                                    children: <Widget>[
+                                                      const Text('This is a typical dialog.'),
+                                                      const SizedBox(height: 15),
+                                                      TextButton(
+                                                        onPressed: () {
+                                                          Navigator.pop(context);
+                                                        },
+                                                        child: const Text('Close'),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ), 
+                                            ),
+                                            ),
+                                            TextButton(
+                                              child: const Text('Регистрация'),
+                                              onPressed: () => showDialog<String>(
+                                              context: context,
+                                              builder: (BuildContext context) => Dialog(
+                                                child: Padding(
+                                                  padding: const EdgeInsets.all(8.0),
+                                                  child: Column(
+                                                    mainAxisSize: MainAxisSize.min,
+                                                    mainAxisAlignment: MainAxisAlignment.center,
+                                                    children: <Widget>[
+                                                      const Text('This is a typical dialog.'),
+                                                      const SizedBox(height: 15),
+                                                      TextButton(
+                                                        onPressed: () {
+                                                          Navigator.pop(context);
+                                                        },
+                                                        child: const Text('Close'),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ), 
+                                            ),
+                                            ),
+                                            
+                                          ]
+                                        )
+                                      ]
                                     )
-                                    
+                                                                   
                                   ],
                                 ),
+                                
                               ],
                             )
                           )
