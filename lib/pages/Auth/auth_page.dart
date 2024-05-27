27409fb0ff322994db9c094fb0b6da6e853947f7
weidermartenn <weidermartenn@gmail.com>
2024-05-27 18:22:24 +0300

    sad

--- lib/pages/Auth/auth_page.dart
+++ lib/pages/Auth/auth_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:Employment_Service/pages/Main/main_app.dart';
+import 'package:Employment_Service/scripts/dialogs.dart';
 
 class AuthPage extends StatefulWidget {
   const AuthPage({super.key});
@@ -154,7 +155,7 @@ class _AuthPageState extends State<AuthPage>{
                                                 
                                               }
                                               else {
-                                                incorrectInputDialog(context);
+                                                emptyInputDialog(context);
                                               }
                                             },
                                             style: ElevatedButton.styleFrom(
@@ -194,46 +195,5 @@ class _AuthPageState extends State<AuthPage>{
   }
 }
 
-incorrectInputDialog(BuildContext dialogContext){ 
-  Widget okButton = TextButton( 
-    onPressed: () {
-      Navigator.of(dialogContext, rootNavigator: true).pop();
-    },
-    style: TextButton.styleFrom(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      )
-    ), 
-    child: const Text( 
-      'ОК',
-      style: TextStyle(
-        color: Color.fromARGB(184, 0, 0, 0),
-        fontWeight: FontWeight.w600,
-      ),
-    ),
-  );
-  AlertDialog alert = AlertDialog( 
-    title: Image.asset( 
-      'assets/icons/black/incorrect_icon_black.png',
-      width: 40,
-      height: 40,
-    ),
-    content: const Text( 
-      'Пожалуйста, заполните форму.',
-      style: TextStyle( 
-        fontSize: 16,
-      )
-    ), 
-    actionsAlignment: MainAxisAlignment.center,
-    actions: [
-      okButton,
-    ],
-  );
-  showDialog( 
-    context: dialogContext,
-    builder: (BuildContext context){ 
-      return alert;
-    },
-  );
-}
+
   
