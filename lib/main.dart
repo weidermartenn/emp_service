0414534afefaad99c73d308a8092f1c723c02d4a
Oleg Krivyonishev <leshiy.vl5@gmail.com>
2024-05-28 14:43:46 +0300

    ghf

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'pages/Main/main_app.dart';
+import 'package:Employment_Service/pages/Admin/admin_page.dart';
 
 
 void main() => runApp(const MaterialApp(
-  home: MainApp()
+  home: AdminPage()
 ));
 
 
