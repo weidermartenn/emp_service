48d0d3ad12ce816be363fe3aac9cafb36bf118d1
weidermartenn <weidermartenn@gmail.com>
2024-05-31 01:18:39 +0300

    ended

--- lib/pages/User/user_page.dart
+++ lib/pages/User/user_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -33,7 +35,8 @@ class _UserPageState extends State<UserPage> {
           focusedBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Color.fromARGB(90, 255, 255, 255)),
           ),
-        )
+        ),
+       
       ),
       home: Scaffold(
         body: LayoutBuilder(builder: (context, constraints) {
@@ -65,6 +68,11 @@ class _UserPageState extends State<UserPage> {
                           ),
                         ),
                       ),
+                      TextButton( 
+                        onPressed: () {},
+                        child: const Text('Пособия', style: TextStyle(color: Colors.white, fontSize: 20)),
+                      ),
+                      const SizedBox(width: 50,),
                       Container(
                         margin: const EdgeInsets.only(right: 10),
                         child: ConstrainedBox(
@@ -109,92 +117,271 @@ class _UserPageState extends State<UserPage> {
                   ),
                 ),
                 const Divider(thickness: 1, color: Colors.white),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                const SizedBox(height: 15),
+                Row( 
                   children: [
                     Container(
                       decoration: const BoxDecoration(
                         borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Color.fromARGB(255, 31, 31, 31),
+                        color: Color.fromARGB(255, 22, 22, 22),
                       ),
                       margin: const EdgeInsets.only(left: 20),
-                      width: 200,
+                      width: 290,
                       height: 500,
                       // Sorting container
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            Column( 
+                            Container( 
+                              width: 250,
+                              decoration: BoxDecoration(
+                                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                                color: const Color.fromARGB(255, 39, 39, 39),
+                                border: Border.all(color: Colors.white, width: 1),
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: Column( 
                               children: [ 
                                 const Text( 
-                                'Сортировка',
+                                'Сортировка зарлплат',
                                 style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: 15,
+                                  fontSize: 16,
                                 ),
                               ),
-                              DropdownButton<String>(
-                                dropdownColor: Color.fromARGB(255, 31, 31, 31),
-                                items: const[
-                                  DropdownMenuItem<String>(
-                                    value: 'asc',
-                                    child: Text('Sort Ascending', style: TextStyle(color: Colors.white)),
+                              const SizedBox(height: 10),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      // Sorting logic for ascending
+                                    },
+                                    style: ButtonStyle(
+                                      overlayColor: MaterialStateProperty.all<Color>(const Color.fromARGB(96, 146, 146, 146)),
+                                      fixedSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+                                      elevation: MaterialStateProperty.all<double>(2.0),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(5.0),
+                                        ),
+                                      ),
+                                    ),
+                                    child: const Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.arrow_upward,
+                                          color: Colors.white,
+                                        ),
+                                        SizedBox(width: 8),
+                                        Text(
+                                          'К наибольшему',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                  DropdownMenuItem<String>(
-                                    value: 'desc',
-                                    child: Text('Sort Descending'),
+                                  TextButton(
+                                    onPressed: () {
+                                      // Sorting logic for descending
+                                    },
+                                    style: ButtonStyle(
+                                      overlayColor: MaterialStateProperty.all<Color>(const Color.fromARGB(96, 146, 146, 146)),
+                                      fixedSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+                                      elevation: MaterialStateProperty.all<double>(2.0),
+                                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(5.0),
+                                        ),
+                                      ),
+                                    ),
+                                    child: const Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.arrow_downward,
+                                          color: Colors.white,
+                                        ),
+                                        SizedBox(width: 8),
+                                        Text(
+                                          'К наименьшему',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ],
-                                onChanged: (value) {
-                                  // Sorting logic here
-                                },
                               ),
                             ],
                           ),
-                          Column( 
-                            children: [ 
-                              const Text( 
-                                'Диапазон зарплат', 
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15,
-                                ),
-                              ),
-                              Slider(
-                                thumbColor: Color.fromARGB(255, 56, 93, 160),
-                                activeColor: Color.fromARGB(255, 56, 93, 160),
-                                min: 0,
-                                max: 1000000,
-                                onChanged: (value) {
-                                  // Range selection logic here
-                                },
-                                value: 500000,
-                              ),
-                            ],
                           ),
-                          Column( 
-                            children: [
-                              const Text( 
-                                'Конкретное значение', 
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 15,
+                          Container( 
+                            width: 250,
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.all(Radius.circular(10)),
+                              color: const Color.fromARGB(255, 39, 39, 39),
+                              border: Border.all(color: Colors.white, width: 1),
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: Column( 
+                              children: [
+                                const Text( 
+                                  'Зарлпаты', 
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Row( 
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [ 
+                                    const Text('от', style: TextStyle(color: Colors.white, fontSize: 15)),
+                                    const SizedBox(width: 8),
+                                    ConstrainedBox(
+                                      constraints: const BoxConstraints.tightFor(width: 80, height: 40),
+                                      child: TextField(
+                                        style: const TextStyle(color: Colors.white, fontSize: 14),
+                                        keyboardType: TextInputType.number,
+                                        textAlignVertical: TextAlignVertical.top,
+                                        onChanged: (value) {
+                                          // Logic for handling input changes
+                                        },
+                                      ),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    const Text('до', style: TextStyle(color: Colors.white, fontSize: 15)),
+                                    const SizedBox(width: 8),
+                                    ConstrainedBox(
+                                      constraints: const BoxConstraints.tightFor(width: 80, height: 40),
+                                      child: TextField(
+                                        style: const TextStyle(color: Colors.white, fontSize: 14),
+                                        keyboardType: TextInputType.number,
+                                        textAlignVertical: TextAlignVertical.top,
+                                        onChanged: (value) {
+                                          // Logic for handling input changes
+                                        },
+                                      ),
+                                    ),
+                                    const SizedBox(width: 8),
+                                  ],
+                                )
+                              ],
+                            )
+                          ),
+                          Container( 
+                            width: 250,
+                            decoration: BoxDecoration(
+                              borderRadius: const BorderRadius.all(Radius.circular(10)),
+                              color: const Color.fromARGB(255, 39, 39, 39),
+                              border: Border.all(color: Colors.white, width: 1),
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: ElevatedButton( 
+                              onPressed: () {
+                                // Logic for handling button click
+                              },
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(const Color.fromARGB(255, 34, 34, 34)),
+                                overlayColor: MaterialStateProperty.all<Color>(const Color.fromARGB(95, 97, 97, 97)),
+                                fixedSize: MaterialStateProperty.all<Size>(const Size(200, 50)),
+                                elevation: MaterialStateProperty.all<double>(2.0),
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                  ),
                                 ),
                               ),
-                              ConstrainedBox(constraints: const BoxConstraints.tightFor(width: 100, height: 50),
-                                child: TextField(  
-                                  style: const TextStyle(color: Colors.white, fontSize: 15),                         
-                                  keyboardType: TextInputType.number,
-                                  onChanged: (value) {
-                                },
+                              child: const Text( 
+                                'Вывести компании с высоким рейтингом',
+                                style: TextStyle(color: Colors.white),
+                                textAlign: TextAlign.center,
                               ),
-                            ) 
-                            ],
+                            )
                           )
-                          
                         ],
                       ),
                     ),
+                    const SizedBox(width: 130),
+                    Container( 
+                      width: 700,
+                      height: 500,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(Radius.circular(10)),
+                        color: const Color.fromARGB(255, 39, 39, 39),
+                        border: Border.all(color: Colors.white, width: 1),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Column( 
+                        children: [
+                          const Text('Количество активных вакансий: 0', style: TextStyle(color: Colors.white, fontSize: 15)),
+                          const SizedBox(height: 10),
+                          Expanded( 
+                            child: ListView.builder( 
+                              itemCount: 5,
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+                              physics: const ClampingScrollPhysics(),
+                              itemBuilder:(context, index) { 
+                                return ListTile( 
+                                  title: const Text('Вакансия 1', style: TextStyle(color: Colors.white)),
+                                  subtitle: Column( 
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Container( 
+                                        width: 600,
+                                        padding: const EdgeInsets.all(10),
+                                        decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(5)),
+                                          border: Border.all(color: Colors.white, width: 1),
+                                        ),
+                                        child: Row( 
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [ 
+                                            Column( 
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [ 
+                                                const Text('Описание: Описание вакансии', style: TextStyle(color: Colors.white)),
+                                                GestureDetector( 
+                                                  onTap: () { 
+                                                    showModalBottomSheet( 
+                                                      context: context, 
+                                                      builder: (BuildContext context) { 
+                                                        return Container( 
+                                                          width: 500,
+                                                          decoration: BoxDecoration(
+                                                            color: const Color.fromARGB(255, 39, 39, 39),
+                                                          ),
+                                                          child: const Column( 
+                                                            children: [ 
+                                                              Text('Компания 1', style: TextStyle(color: Colors.white)),
+                                                              Text('Описание компании', style: TextStyle(color: Colors.white)),
+                                                            ],
+                                                          )
+                                                        );
+                                                      },
+                                                    );
+                                                  },
+                                                  child: const Text('Компания: Компания 1', style: TextStyle(color: Colors.white)),
+                                                ),
+                                                const Text('Рейтинг: 4.5', style: TextStyle(color: Colors.white)),
+                                              ]
+                                            ),
+                                            const Text('39824 руб.', style: TextStyle(color: Colors.white, fontSize: 20)),
+                                          ]
+                                        )
+                                      )
+                                    ],
+                                  )
+                                );
+                              },
+                            )
+                          )
+                        ],
+                      )
+                    )
                   ],
                 )
               ],
