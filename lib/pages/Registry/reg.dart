9c4b9f58765a88edff5a7fed3bc5a09ffa39cce2
Oleg Krivyonishev <leshiy.vl5@gmail.com>
2024-05-31 10:18:33 +0300

    df

--- lib/pages/Registry/reg.dart
+++ lib/pages/Registry/reg.dart
@@ -1,6 +1,10 @@
 import 'package:postgres/postgres.dart';
 
-Registry(user, phone, pass, rep_pas) async {
+Future<String> Registry(user, phone, pass, rep_pas) async {
+  final _user = user.toString();
+  final _phone = phone.toString();
+  final _pass = pass.toString();
+  final _rep_pas = rep_pas.toString();
   final conn = await Connection.open( 
     Endpoint( 
       host: 'localhost',
@@ -11,6 +15,13 @@ Registry(user, phone, pass, rep_pas) async {
     settings: ConnectionSettings(sslMode: SslMode.disable)
   );
 
-  final result = await conn.execute();
+  final result = await conn.execute(
+    "select * from employment_service.check_reg_correct('$_user', '$_phone', '$_pass', '$_rep_pas')"
+  );
+  return result[0][0].toString();
 
+}
+
+void main() async{
+  print(await Registry('user45', 'test', 'pass', 'test'));
 }
